bb4d5ee2a6cb6876f2487a80499142b495aa10d8
T Prawin Kingstan <tprawinkingstan@gmail.com>
2026-02-15 05:44:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7460476619-45a00",
+  "appId": "1:351467731615:web:b032c96d8297b08e3880aa",
+  "apiKey": "AIzaSyBm9L7EZSQiVKRYF9dFJNGyXGJTm77JMXM",
+  "authDomain": "studio-7460476619-45a00.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "351467731615"
+};
